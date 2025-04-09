5b74a6b6792fba41705f7bb6c467ccbcb08438d4
Mobina Poulaei <m.poulaei@gmail.com>
2025-04-09 06:43:51 +0330

    Update eval_sde_adv.py

--- eval_sde_adv.py
+++ eval_sde_adv.py
@@ -136,7 +136,7 @@ def eval_autoattack(args, config, model, x_val, y_val, adv_batch_size, log_dir):
     print(f'apply the attack to sde_adv [{args.lp_norm}]...')
     model_.reset_counter()
     adversary_sde = AutoAttack(model, norm=args.lp_norm, eps=args.adv_eps,
-                               version=attack_version, attacks_to_run=attack_list,
+                               version=attack_version, attacks_to_run=[],
                                log_path=f'{log_dir}/log_sde_adv.txt', device=config.device)
     if attack_version == 'custom':
         adversary_sde.apgd.n_restarts = 1
